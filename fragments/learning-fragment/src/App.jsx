799671af50b25dbd32643ad8f46f0d3c72d2dbcb
realsoumyadey07 <soumyadipdey802@gmail.com>
2023-12-14 14:28:51 +0530

    before props

--- fragments/learning-fragment/src/App.jsx
+++ fragments/learning-fragment/src/App.jsx
@@ -1,12 +1,20 @@
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
 
+  // let foodItems = []
+  let foodItems=["Dals","Green vegetables","Milk","Salads","Ghee","Roti"]
+  let emptyItems = foodItems.length===0? <h3>I am still hungry</h3>:null
 
   return (
     <>
-      
+        <center>Healthy Food</center>
+        {emptyItems}
+        <ul className="list-group">
+        {foodItems.map((e)=><li className="list-group-item" key={e}>{e}</li>)}  
+        </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
