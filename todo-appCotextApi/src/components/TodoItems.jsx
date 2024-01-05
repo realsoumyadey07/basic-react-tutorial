69992fcbcd84aa837c81e59d252b668db77dd127
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-01-05 18:13:59 +0530

    on going

--- todo-appCotextApi/src/components/TodoItems.jsx
+++ todo-appCotextApi/src/components/TodoItems.jsx
@@ -2,12 +2,19 @@ import { useContext } from "react";
 import { TodoItemsContext } from "../store/todo-items-store";
 import TodoItem from "./TodoItem";
 
-export default function TodoItems({onDeleteClick}) {
-  const {todoItems}= useContext(TodoItemsContext); 
+export default function TodoItems({ onDeleteClick }) {
+  const { todoItems } = useContext(TodoItemsContext);
   return (
     <>
       <div className="items-container">
-      {todoItems.map(e=><TodoItem todoDate={e.dueDate} onDeleteClick={onDeleteClick}  key={e.name} todoName={e.name}/>)}
+        {todoItems.map((e) => (
+          <TodoItem
+            todoDate={e.dueDate}
+            onDeleteClick={onDeleteClick}
+            key={e.name}
+            todoName={e.name}
+          />
+        ))}
       </div>
     </>
   );
